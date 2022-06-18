dcfad6b2099c9b4e9aef802f1bbf617068caa7e1
Jesica <13519011@std.stei.itb.ac.id>
2022-06-18 18:48:43 +0700

    responsive sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,53 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { SideBarItem } from './SidebarItem';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { SideBarItem } from "./SidebarItem";
+import { FaTimes, FaBars } from "react-icons/fa";
 
 export default function Sidebar() {
-  const [currentPath, setCurrentPath] = useState('')
   const [selectedId, setSelectedId] = useState(
-    window.location.pathname === '/' ? 0 : 4 // TODO
-  )
-  
+    window.location.pathname === "/" ? 0 : 4 // TODO
+  );
+  const [barsClicked, setBarsClicked] = useState(false)
+
+  const barsHandler = () => {
+    setBarsClicked(!barsClicked)
+  }
+
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 m-0 
-        flex flex-col 
-        bg-white text-[grey] shadow-xl'>
-        {
-          SideBarItem.map((item, index) => {
-            return <SideBarIcon key={index} icon={item.icon} to={item.to} onClick={() => setSelectedId(index)} className={
-              selectedId === index ? 'sidebar-icon-selected' : 'sidebar-icon'
-            }/>
-          })
-        }
-    </div>
+    <>
+      <div className="md:hidden m-0 p-o fixed h-16 top-0 left-0 w-full bg-primary shadow-lg flex justify-between px-5 py-[1.2rem] rounded-b-sm">
+        <h1 className="text-white text-lg font-semibold">Company Name</h1>
+        <i className="text-white cursor-pointer" onClick={barsHandler}>
+          {barsClicked ? <FaTimes size='28'/> : <FaBars size="28" />}
+        </i>
+      </div>
+      <ul
+        className={barsClicked ? "sidebar active" : "sidebar"}
+      >
+        {SideBarItem.map((item, index) => {
+          return (
+            <SideBarIcon
+              key={index}
+              icon={item.icon}
+              to={item.to}
+              onClick={() => setSelectedId(index)}
+              className={
+                selectedId === index ? "sidebar-icon-selected" : "sidebar-icon"
+              }
+            />
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
-const SideBarIcon = ({icon, to, onClick, className}) => {
-  return(
-    <Link to={to ?? '#'} className={className} onClick={onClick}>
-      {icon}
-    </Link>
-  )
-}
+const SideBarIcon = ({ icon, to, onClick, className }) => {
+  return (
+    <li>  
+      <Link to={to ?? "#"} className={className} onClick={onClick}>
+        {icon}
+      </Link>
+    </li>
+  );
+};
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -6,19 +6,24 @@ import { BsBuilding } from "react-icons/bs";
 export const SideBarItem = [
   {
     icon: <BiHomeCircle size='22'/>,
+    name: 'Home',
     to: '/'
   },
   {
     icon: <HiOutlineTicket size='22'/>,
+    name: 'Item 2',
   },
   {
     icon: <BiTask size='22'/>,
+    name: 'Item 3',
   },
   {
     icon: <IoTrashBinOutline size='22'/>,
+    name: 'Item 4',
   },
   {
     icon: <BsBuilding size='22'/>,
-    to: '/dashboard'
+    to: '/dashboard',
+    name: 'Dashboard',
   },
 ]
